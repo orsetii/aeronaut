78f7686b84992ff6291bb65735cfa6eba1a20f75
Tom French <hello@orseti.com>
2021-02-03 07:45:10 +0000

    Finished beginning of ImGui, with solid base working.

--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -1,68 +1,151 @@
-#include "imgui.h"
-#include "imgui_impl_sdl.h"
-#include "imgui_impl_opengl3.h"
+//#include "imgui.h"
+//#include "imgui_impl_glfw.h"
+//#include "imgui_impl_opengl3.h"
+
+#include "../imgui/imgui.h"
+#include "../imgui/imgui_impl_sdl.h"
+#include "../imgui/imgui_impl_opengl3.h"
 #include <stdio.h>
+#include <SDL2/SDL.h>
+#include "aerogui.h"
+#include "imgui-style.h"
+#include <string>
+#include <iostream>
+
+
+#if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
 #include <GL/gl3w.h>            // Initialize with gl3wInit()
-#include <SDL.h>
-
-
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
+#include <GL/glew.h>            // Initialize with glewInit()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
+#include <glad/glad.h>          // Initialize with gladLoadGL()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD2)
+#include <glad/gl.h>            // Initialize with gladLoadGL(...) or gladLoaderLoadGL()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING2)
+#define GLFW_INCLUDE_NONE       // GLFW including OpenGL headers causes ambiguity or multiple definition errors.
+#include <glbinding/Binding.h>  // Initialize with glbinding::Binding::initialize()
+#include <glbinding/gl/gl.h>
+using namespace gl;
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING3)
+#define GLFW_INCLUDE_NONE       // GLFW including OpenGL headers causes ambiguity or multiple definition errors.
+#include <glbinding/glbinding.h>// Initialize with glbinding::initialize()
+#include <glbinding/gl/gl.h>
+using namespace gl;
+#else
+#include IMGUI_IMPL_OPENGL_LOADER_CUSTOM
+#endif
 
 int main (int argc, char** argv) {
 
+	// H, W of the SDL Window
+	int windowWidth = 1280;
+	int windowHeight = 960;
 
 	// Setup SDL
-	if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
-
-	printf("Error in SDL: %s\n", SDL_GetError());
-	return -1;
+	// (Some versions of SDL before <2.0.10 appears to have performance/stalling issues on a minority of Windows systems,
+	// depending on whether SDL_INIT_GAMECONTROLLER is enabled or disabled.. updating to latest version of SDL is recommended!)
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		printf("Error: %s\n", SDL_GetError());
+		return -1;
 	}
 
-
-	// GL 3.0 + GLSL 130
-	const char* glsl_version = "#version 130";
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-
-	// Create window with graphics context
+	// set OpenGL attributes
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-	SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-	// TODO implement customized title for window, maybe update later?
-	SDL_Window* window = SDL_CreateWindow("Aeronaut C2 Server", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 960, window_flags);
+
+	SDL_GL_SetAttribute(
+	SDL_GL_CONTEXT_PROFILE_MASK,
+	SDL_GL_CONTEXT_PROFILE_CORE
+	);
+
+	std::string glsl_version = "";
+#ifdef __APPLE__
+	// GL 3.2 Core + GLSL 150
+	glsl_version = "#version 150";
+	SDL_GL_SetAttribute( // required on Mac OS
+	SDL_GL_CONTEXT_FLAGS,
+	SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG
+	);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+#elif __linux__
+	// GL 3.2 Core + GLSL 150
+	glsl_version = "#version 150";
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0); 
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+#elif _WIN32
+	// GL 3.0 + GLSL 130
+	glsl_version = "#version 130";
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0); 
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+#endif
+
+	SDL_WindowFlags window_flags = (SDL_WindowFlags)(
+	SDL_WINDOW_OPENGL
+	| SDL_WINDOW_RESIZABLE
+	| SDL_WINDOW_ALLOW_HIGHDPI
+	);
+
+	SDL_Window *window = SDL_CreateWindow(
+		"Aeronaut C2 Server GUI",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		windowWidth,
+		windowHeight,
+		window_flags
+	);
+	// limit to which minimum size user can resize the window
+	SDL_SetWindowMinimumSize(window, 800, 600);
+
 	SDL_GLContext gl_context = SDL_GL_CreateContext(window);
 	SDL_GL_MakeCurrent(window, gl_context);
-	SDL_GL_SetSwapInterval(1); // Enable vsync
 
+	// enable VSync
+	SDL_GL_SetSwapInterval(1);
 	// Initialize OpenGL loader
+#if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
 	bool err = gl3wInit() != 0;
-
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
+	bool err = glewInit() != GLEW_OK;
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
+	bool err = gladLoadGL() == 0;
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD2)
+	bool err = gladLoadGL(glfwGetProcAddress) == 0; // glad2 recommend using the windowing library loader instead of the (optionally) bundled one.
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING2)
+	bool err = false;
+	glbinding::Binding::initialize();
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING3)
+	bool err = false;
+	glbinding::initialize([](const char* name) { return (glbinding::ProcAddress)glfwGetProcAddress(name); });
+#else
+	bool err = false; // If you use IMGUI_IMPL_OPENGL_LOADER_CUSTOM, your loader is likely to requires some form of initialization.
+#endif
 	if(err) {
 		fprintf(stderr, "Failed to initialize OpenGL loader!\n");
 		return 1;
 	}
+
 
 	// Setup Dear ImGui context
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 	// We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
 	// because it would be confusing to have two docking targets within each others.
-	ImGuiWindowFlags imgui_window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
 
 	// Setup Io and add our flags.
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
+
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
 	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
-	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
-	//io.ConfigFlags |= ImGuiConfigFlags_MenuBar;         // Enable Multi-Viewport / Platform Windows
+	//io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
 
-	// Setup GUI style
+	// Setup GUI style from "imgui-style.h"
 	ImGui::StyleColorsDark();
-
-
 	ImGuiStyle& style = ImGui::GetStyle();
+
 	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
 		style.WindowRounding = 0.0f;
 		style.Colors[ImGuiCol_WindowBg].w = 1.0f;
@@ -70,64 +153,111 @@ int main (int argc, char** argv) {
 
 	// Setup Platform/Renderer backends
 	ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
-	ImGui_ImplOpenGL3_Init(glsl_version);
+	ImGui_ImplOpenGL3_Init(glsl_version.c_str());
+
+	// colors are set in RGBA, but as float
+	ImVec4 background = ImVec4(35/255.0f, 35/255.0f, 35/255.0f, 1.00f);
+
+	glClearColor(background.x, background.y, background.z, background.w);
 
 	// Setup default font
-	io.Fonts->AddFontDefault();
-
-    	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
-
-
-
+	//io.Fonts->AddFontDefault();
 
 	// The Main GUI Event Loop
 	// Polls and handles events.
 	bool done = false;
 	while (!done) {
 
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+
+		ImGuiWindowFlags imgui_window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoDecoration
+			| ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoBringToFrontOnFocus   ;
+		// Enable Dockspace at start
+		static bool dockspaceOpen = true;
+
+		// Poll and handle events (inputs, window resize, etc.)
+		// You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+		// - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
+		// - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
+		// Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
 		SDL_Event event;
 		while (SDL_PollEvent(&event)) {
+		    // without it you won't have keyboard input and other things
 		    ImGui_ImplSDL2_ProcessEvent(&event);
-		    if (event.type == SDL_QUIT)
-			done = true;
-		    if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window))
-			done = true;
-		}
+		    // you might also want to check io.WantCaptureMouse and io.WantCaptureKeyboard
+		    // before processing events
+		    
+		    switch (event.type)
+		    {
+			    case SDL_QUIT:
+				done = true;
+				break;
 
-		// Start the ImGui frame
+			    case SDL_WINDOWEVENT:
+				switch (event.window.event)
+				{
+				case SDL_WINDOWEVENT_RESIZED:
+				    windowWidth = event.window.data1;
+				    windowHeight = event.window.data2;
+				    // std::cout << "[INFO] Window size: "
+				    //           << windowWidth
+				    //           << "x"
+				    //           << windowHeight
+				    //           << std::endl;
+				    glViewport(0, 0, windowWidth, windowHeight);
+				    break;
+				}
+				break;
+
+			    case SDL_KEYDOWN:
+				switch (event.key.keysym.sym) {
+				case SDLK_ESCAPE:
+				   done = true;	
+				   break;
+				}
+				break;
+            		}
+        	}
+
+
+		// Start the Dear ImGui frame
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplSDL2_NewFrame(window);
 		ImGui::NewFrame();
 
-		// Enable Dockspace at start
-		static bool dockspaceOpen = true;
-		static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
-		// When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background and handle the pass-thru hole, so we ask Begin() to not render a background.
-		if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
-			imgui_window_flags |= ImGuiWindowFlags_NoBackground;
+		{
+
+			int sdl_width = 0, sdl_height = 0;
+			SDL_GetWindowSize(window, &sdl_width, &sdl_height);
+			int controls_width = sdl_width;
+			if ((controls_width /= 3) < 500) { controls_width = 500; }
+			// make main widget width to be 1:1 of the main window dimensions
+
+			// position the controls widget in the top-right corner with some margin
+			ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_Always);
+			ImGui::SetNextWindowSize(
+			ImVec2(static_cast<float>(sdl_width), static_cast<float>(sdl_height)),
+				ImGuiCond_Always
+			);
+			// here we set the calculated width and also make the height to be
+			// be the height of the main window also with some margin
+		}
 
 		// Important: note that we proceed even if Begin() returns false (aka window is collapsed).
 		// This is because we want to keep our DockSpace() active. If a DockSpace() is inactive, 
 		// all active windows docked into it will lose their parent and become undocked.
 		// We cannot preserve the docking relationship between an active window and an inactive docking, otherwise 
 		// any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
-		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
 		ImGui::Begin("DockSpace Demo", &dockspaceOpen, imgui_window_flags);
-		ImGui::PopStyleVar();
 
 		// DockSpace
 		ImGuiIO& io = ImGui::GetIO();
-		ImGuiStyle& style = ImGui::GetStyle();
-		float minWinSizeX = style.WindowMinSize.x;
-		style.WindowMinSize.x = 370.0f;
 		if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
 		{
-			ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
-			ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+			ImGuiID dockspace_id = ImGui::GetID("DockSpace");
+			ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), 0);
 		}
-
-		style.WindowMinSize.x = minWinSizeX;
 
 		if (ImGui::BeginMenuBar())
 		{
@@ -147,31 +277,64 @@ int main (int argc, char** argv) {
 			}
 
 			ImGui::EndMenuBar();
-		}
-
+		}// a window is defined by Begin/End pair
+       
 		// Note ImGui::Text("Draw Calls: %d", stats.DrawCalls) can do printf stuff.
 
 		ImGui::End();
 
+		// a window is defined by Begin/End pair
+		{
+		    static int counter = 0;
+		    // get the window size as a base for calculating widgets geometry
+		  		    // create a window and append into it
+		    ImGui::Begin("Controls", NULL, ImGuiWindowFlags_None);
+
+		    ImGui::Dummy(ImVec2(0.0f, 1.0f));
+		    ImGui::TextColored(ImVec4(1.0f, 0.0f, 1.0f, 1.0f), "Platform");
+		    ImGui::Text("%s", SDL_GetPlatform());
+		    ImGui::Text("CPU cores: %d", SDL_GetCPUCount());
+		    ImGui::Text("RAM: %.2f GB", SDL_GetSystemRAM() / 1024.0f);
+		    
+		    // buttons and most other widgets return true when clicked/edited/activated
+		    if (ImGui::Button("Counter button"))
+		    {
+			std::cout << "counter button clicked\n";
+			counter++;
+		    }
+		    ImGui::SameLine();
+		    ImGui::Text("counter = %d", counter);
+
+		    ImGui::End();
+		}
+
+		// Redraw Background
+		SDL_GetWindowSize(window, &windowWidth, &windowHeight);
+
 
 		// Rendering
 		ImGui::Render();
-		glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
-		glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-		glClear(GL_COLOR_BUFFER_BIT);
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
 		// Update and Render additional Platform Windows
 		// (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
 		//  For this specific demo app we could also call SDL_GL_MakeCurrent(window, gl_context) directly)
-		SDL_Window* backup_current_window = SDL_GL_GetCurrentWindow();
-		SDL_GLContext backup_current_context = SDL_GL_GetCurrentContext();
-		ImGui::UpdatePlatformWindows();
-		ImGui::RenderPlatformWindowsDefault();
-		SDL_GL_MakeCurrent(backup_current_window, backup_current_context);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		
+		// Update and Render additional Platform Windows
+		// (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
+		//  For this specific demo app we could also call glfwMakeContextCurrent(window) directly)
+		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+		{
+			SDL_Window* backup_current_window = SDL_GL_GetCurrentWindow();
+			SDL_GLContext backup_current_context = SDL_GL_GetCurrentContext();
+			ImGui::UpdatePlatformWindows();
+			ImGui::RenderPlatformWindowsDefault();
+			SDL_GL_MakeCurrent(backup_current_window, backup_current_context);
+		}
 
 		SDL_GL_SwapWindow(window);
-    }
+
+
+	}
 
     // Cleanup
     ImGui_ImplOpenGL3_Shutdown();
@@ -183,9 +346,5 @@ int main (int argc, char** argv) {
     SDL_Quit();
 
     return 0;
-
-
-
-
 }
 
