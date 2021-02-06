c0a843c29594bd12835279bcc2bd0bae2a5e88b5
Tom French <hello@orseti.com>
2021-02-06 19:23:54 +0000

    Stuff

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,18 @@
 #include <thread>
 #include <future>
 
-
-
-int main(int argc, char** argv) {
-
-
+int main(int argc, char **argv)
+{
 
 	// Server Thread
-	auto ServerExit = std::async(std::launch::async, ServerRun, "0.0.0.0", 8080, ".");
+	auto ServerExit =
+	    std::async(std::launch::async, ServerRun, "0.0.0.0", 8080, ".");
 
 	Gui gui(1280, 960);
 	// Gui Thread
-	// atm the GUI exiting exits the entire process. This could be changed? not sure.
-	std::future<int> ret = std::async(&Gui::Run, &gui);
+	// atm the GUI exiting exits the entire process. This could be
+	// changed? not sure.
+	std::future < int >ret = std::async(&Gui::Run, &gui);
 
 	int err = ret.get();
 	if (err) {
